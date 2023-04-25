9d1799cf4cd329b355a7907bbaf69b336e76d5ea
AlexCsorba <124453983+AlexCsorba@users.noreply.github.com>
2023-04-25 15:48:47 -0400

    ComparableComparator

--- src/wol/ComparableComparator.java
+++ src/wol/ComparableComparator.java
@@ -1,4 +1,22 @@
 package wol;
 
-public class ComparableComparator<T>{
+import java.util.Comparator;
+public class ComparableComparator<T> implements Comparator<T> {
+
+
+//     Default constructor. Does nothing really.
+//
+    public ComparableComparator() {
+        // nothing to do
+    }
+
+
+//     Compares the two specified objects using their natural ordering.
+//
+//     lhs the first object to be compared
+//     rhs the second object to be compared
+//
+    public int compare(T lhs, T rhs) {
+        return lhs.compareTo(rhs);
+    }
 }
